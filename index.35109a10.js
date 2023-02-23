ff21621b1cf052fa7e1eddb9626658a29c66a600
LukaszCiaston-coder <119055078+LukaszCiaston-coder@users.noreply.github.com>
2023-02-23 15:16:02 +0000

    Deploying to gh-pages from @ LukaszCiaston-coder/team-project-icecream@4abb78c27e85ac8a814d2b352aa9626d6bafe5c2 🚀

--- index.35109a10.js
+++ index.35109a10.js
@@ -1,2 +1,2 @@
 (()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((e=>e.addEventListener("click",o))),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.4470a6a2.js.map
+//# sourceMappingURL=index.35109a10.js.map
